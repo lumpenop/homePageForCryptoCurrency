944b3abb71615412e53014af307fa55d2b041f36
osy1115 <godtttever@naver.com>
2021-05-17 14:56:37 +0900

    sh js complete

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const mainRouter = require('./main/index');
+const mainUser = require('./user'); 
 
 
+router.use('/user',mainUser);
 router.use('/', mainRouter);
 
 module.exports = router; 
