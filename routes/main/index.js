f4beadb16e31102e0cc92f3fc540aa9d3a827a7a
jihyun <kjwise1128@gmail.com>
2021-05-17 18:28:05 +0900

    개어려워~~~~

--- routes/main/index.js
+++ routes/main/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('./main.controller');
 
+
 router.get('/', controller.mainCon);
+
+
 
 
 module.exports = router;
